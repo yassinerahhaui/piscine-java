151697fd9de797abb462eac9d24fa73caacf01e5
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-04 05:37:12 +0100

    add HelloWorld exercise solution

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,4 +2,7 @@ public class HelloWorld {
     public static String helloWorld() {
         return "Hello World!";
     }
+    public static void main(String[] args) {
+        System.out.println(HelloWorld.helloWorld());
+    }
 }
