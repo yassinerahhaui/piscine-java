7552b289195f2b52a7c31695d8d12ebec8fa8782
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-05 03:47:17 +0100

    add Chifoumi exercise solution

---
+++ Chifoumi/Chifoumi.java
@@ -0,0 +1,11 @@
+// package Chifoumi;
+
+public class Chifoumi {
+    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
+        return switch (chifoumiAction) {
+            case ROCK -> ChifoumiAction.SCISSOR;
+            case PAPER -> ChifoumiAction.ROCK;
+            case SCISSOR -> ChifoumiAction.PAPER;
+        };
+    }
+}
