59e824a2723b220fe1a1dca5ad26cf222705b0b7
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-05 01:22:17 +0100

    add FloatOperations exercise solution

---
+++  FloatOperations/FloatOperations.java
@@ -0,0 +1,10 @@
+
+
+public class FloatOperations {
+    public static float addTwoFloats(float a, float b) {
+        return a + b; 
+    }
+    public static float divideTwoFloats(float a, float b) {
+        return a * b;
+    }
+}
