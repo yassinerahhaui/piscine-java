12a3deb993843214f37f933428305400f682b67f
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-05 04:08:25 +0100

    add StringConcat exercise solution

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -2,6 +2,9 @@
 
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null) {
+            return s2;
+        }
         return s1.concat(s2);
     }
 }
