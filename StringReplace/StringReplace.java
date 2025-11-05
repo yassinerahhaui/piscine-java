91bc19a907291c45bb85c590a57cf5ace4c1fe6e
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-05 05:19:56 +0100

    add StringReplace exercise solution

---
+++ StringReplace/StringReplace.java
@@ -0,0 +1,11 @@
+// package StringReplace;
+
+public class StringReplace {
+    public static String replace(String s, Character target, Character replacement) {
+        return s.replace(target, replacement);
+    }
+
+    public static String replace(String s, String target, String replacement) {
+        return s.replace(target, replacement);
+    }
+}
