bcfacb97247f6130cb22050a8ef91fffe455b4f6
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-05 10:22:35 +0100

    add DoOp exercise solution

---
+++ DoOp/DoOp.java
@@ -0,0 +1,28 @@
+// package DoOp;
+
+public class DoOp {
+    public static String operate(String[] args) {
+        if (args.length == 0) {
+            return "it depend on the input.";
+        }
+        if (args[2] == "0" && args[1] == "%") {
+            return "Error";
+        }
+        var num = switch (args[1]) {
+            case "+" -> Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
+            case "-" -> Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
+            case "*" -> Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
+            case "/" -> Integer.parseInt(args[0]) / Integer.parseInt(args[2]);
+            case "%" -> Integer.parseInt(args[0]) % Integer.parseInt(args[2]);
+            default -> "Error";
+        };
+        return num.toString();
+    }
+
+    // public static void main(String[] args) {
+    //     System.out.println(DoOp.operate(new String[]{"1","+","2"}));
+    //     System.out.println(DoOp.operate(new String[]{"1","-","1"}));
+    //     System.out.println(DoOp.operate(new String[]{"1","%","0"}));
+    //     System.out.println(DoOp.operate(args));
+    // }
+}
