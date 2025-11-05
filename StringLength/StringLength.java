3d21a75f44f53e72d9dca283f551d5d6745a5077
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-05 04:00:14 +0100

    add StringLength exercise solution

--- StringLength/StringLength.java
+++ StringLength/StringLength.java
@@ -1,7 +1,11 @@
 // package StringLength;
 
+
 public class StringLength {
     public static int getStringLength(String s) {
+        if (s != null) {
+            return 0;
+        }
         return s.length();
     }
 }
