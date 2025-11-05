d0f46c0726662148d3d064688cc770a97b1cdc30
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-05 04:22:18 +0100

    add StringContains exercise solution

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -2,6 +2,9 @@
 
 public class StringContains {
     public static boolean isStringContainedIn(String subString, String s) {
+        if (s == null || subString == null) {
+            return false;
+        }
         return s.contains(subString);
     }
 }
