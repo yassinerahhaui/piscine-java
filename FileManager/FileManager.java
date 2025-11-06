4ac3638a49415ce46b8dc8ab6e26da16af6e6126
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-06 10:04:23 +0100

    aa

--- FileManager/FileManager.java
+++ FileManager/FileManager.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class FileManager {
     public static void createFile(String fileName, String content) throws IOException {
