b0f182b7a6a69032cc8cddf9ba4e0e1b5a8b0b10
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-07 16:18:15 +0100

    add StarGetters exersice solution

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,45 @@
+public class CelestialObject {
+    public double x ,y , z;
+    public String name;
+
+    public CelestialObject() { this("Soleil", 0.0, 0.0, 0.0); }
+
+    public CelestialObject(String name, double x,double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    } 
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
