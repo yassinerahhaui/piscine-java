aec48898994c93cade58315f9afd8fabdc7ec9da
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-07 18:47:09 +0100

    add StarIneritance exircise solution

---
+++ StarInheritance/Star.java
@@ -0,0 +1,11 @@
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public double getMagnitude() {
+        return this.magnitude;
+    }
+
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+}
