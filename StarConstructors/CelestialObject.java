29dff9393495a717ec976591fcc4206df5f97e69
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-07 13:56:30 +0100

    add StarConstructors exersice solution

---
+++ StarConstructors/CelestialObject.java
@@ -0,0 +1,13 @@
+public class CelestialObject {
+    public double x = 0.0;
+    public double y = 0.0;
+    public double z = 0.0;
+    public String name = "Soleil";
+
+    public CelestialObject(double x2,double y2, double z2, String name2) {
+        x = x2;
+        y = y2;
+        z = z2;
+        name = name2;
+    }
+}
