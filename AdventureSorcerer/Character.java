018f597c77ff989aad8dab8fd03475a1a95a703b
yrahhaou <yrahhaou@gmail.com>
2025-11-11 19:05:02 +0100

    add AdventureSorcerer exersice solution

--- AdventureSorcerer/Character.java
+++ AdventureSorcerer/Character.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Character {
     private final int maxHealth;
-    public int currentHealth;
+    private int currentHealth;
     private final String name;
     private static List<Character> allCharacters = new ArrayList<Character>();
 
--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -1,5 +1,5 @@
 public class Sorcerer extends Character implements Healer {
-    int healCapacity;
+    private int healCapacity;
 
     public Sorcerer(String name, int maxHealth, int healCapacity) {
         super(name, maxHealth);
