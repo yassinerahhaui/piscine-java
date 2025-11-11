22b6db755c8c35749db20790bb2777841d398e2b
yrahhaou <yrahhaou@gmail.com>
2025-11-11 19:05:43 +0100

    add AdventureSorcerer exersice solution

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -1,5 +1,5 @@
 public class Sorcerer extends Character implements Healer {
-    private int healCapacity;
+    private final int healCapacity;
 
     public Sorcerer(String name, int maxHealth, int healCapacity) {
         super(name, maxHealth);
