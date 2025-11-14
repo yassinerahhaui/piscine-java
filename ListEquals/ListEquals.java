18d5a64bb6f86013ffe367472327c7130986fcb3
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-14 18:41:34 +0100

    fix ListEquals exersice empty argument

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-        if (list1.isEmpty() || list2.isEmpty() || list1 == null || list2 == null) return false;
+        if (list1 == null || list2 == null || list1.isEmpty() || list2.isEmpty()) return false;
         return list1.equals(list2);
     }
 }
