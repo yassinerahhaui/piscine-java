16b2d6158dc2a5d9c70e5ffe640f71081bee1c7a
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-15 10:19:09 +0100

    fix SortedList bug null input

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -5,10 +5,12 @@ import java.util.stream.Collectors;
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
+        if (list == null) return null;
         return list.stream().sorted().collect(Collectors.toList());
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
+        if (list == null) return null;
         return  list.stream()
                     .sorted(Collections.reverseOrder())
                     .collect(Collectors.toList());
