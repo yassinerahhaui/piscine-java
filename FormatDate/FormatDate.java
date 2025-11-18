e516e869fcb318f896389112e49507727e6066d4
yrahhaou <yassinerahhaoui12@gmail.com>
2025-11-18 22:27:28 +0100

    add FormatDate exersice solution

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -1,3 +1,4 @@
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -7,15 +8,17 @@ import java.util.Locale;
 public class FormatDate {
 
     public static String formatToFullText(LocalDateTime dateTime) {
-    if (dateTime == null) return null;
+        if (dateTime == null) {
+            return null;
+        }
 
-    // Le 22 août de l'an 2021 à 13h25m et 46s
-    return String.format(
-        "Le %1$td %1$tB de l'an %1$tY à %1$tHh%1$tMm et %1$tSs",
-        dateTime
-    );
-}
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+                "'Le' d MMMM 'de l''an' yyyy 'à' HH'h'mm'm' 'et' ss's'",
+                Locale.FRENCH
+        );
 
+        return dateTime.format(formatter);
+    }
 
     public static String formatSimple(LocalDate date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALIAN);
@@ -24,7 +27,9 @@ public class FormatDate {
     }
 
     public static String formatIso(LocalTime time) {
-        if (time == null) return null;
+        if (time == null) {
+            return null;
+        }
 
         // Format: HH:mm:ss.nnnnnnnnn
         // The sample output shows 9-digit nanoseconds with leading zeros
