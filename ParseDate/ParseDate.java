b6944c02a8885e31e1483d5761d34d43d71f3b67
yrahhaou <yrahhaou@gmail.com>
2025-11-18 17:28:16 +0100

    add ParseDate exersice solution

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -31,9 +31,9 @@ public class ParseDate {
         if (isMorning && hour == 12) hour = 0;
         return LocalTime.of(hour,minute,second);
     }
-    public static void main(String[] args) {
-        System.out.println(parseIsoFormat("2022-04-25T20:51:28.709039322"));
-        System.out.println(parseFullTextFormat("monday 25 april 2022"));
-        System.out.println(parseTimeFormat("09 hours in the evening, 07 minutes and 23 seconds"));
-    }
+    // public static void main(String[] args) {
+    //     System.out.println(parseIsoFormat("2022-04-25T20:51:28.709039322"));
+    //     System.out.println(parseFullTextFormat("monday 25 april 2022"));
+    //     System.out.println(parseTimeFormat("09 hours in the evening, 07 minutes and 23 seconds"));
+    // }
 }
